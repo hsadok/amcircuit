8aa46bf582addbec0e68ce99e3607d9044131ace
Hugo Sadok <hugombarreto@gmail.com>
2016-02-06 02:27:29 -0200

    Adding main files

---
+++ include/AMCircuit.h
@@ -0,0 +1,15 @@
+//
+// Created by Hugo Sadok on 2/6/16.
+//
+
+#ifndef AMCIRCUIT_AMCIRCUIT_H
+#define AMCIRCUIT_AMCIRCUIT_H
+
+namespace amcircuit {
+
+
+
+}
+
+#endif //AMCIRCUIT_AMCIRCUIT_H
+
---
+++ src/AMCircuit.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Hugo Sadok on 2/6/16.
+//
+
+#include <iostream>
+#include "AMCircuit.h"
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "argc: " << argc << "    argv[0]: " << argv[0] << std::endl;
+    return 0;
+}
