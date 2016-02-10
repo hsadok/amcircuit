91138977cdba96c2e083c6fd88abe80b597c1042
Hugo Sadok <hugombarreto@gmail.com>
2016-02-09 23:02:40 -0200

    Adding Statement class

--- include/Statement.h
+++ include/Statement.h
@@ -5,12 +5,50 @@
 #ifndef AMCIRCUIT_STATEMENT_H
 #define AMCIRCUIT_STATEMENT_H
 
+#include <string>
+#include <sstream>
+
+#include "AMCircuit.h"
+#include "ResourceHandler.h"
+
 namespace amcircuit {
 
 class Statement {
-public:
+ public:
+  Statement();
+  Statement(const std::string& params);
+  virtual ~Statement() = 0;
+
+  typedef ResourceHandler<Statement> Handler;
+  static Statement::Handler get_statement(std::string params);
+
+ protected:
+  std::stringstream line_stream;
+
+ private:
+  Statement(const Statement& other);
+  Statement& operator=(const Statement& other);
 };
 
-}
+
+// Statement only works with the `TRAN` keyword used for transient analysis
+// It assumes the Adams-Moulton method for integration
+// It also assumes that UIC (Use initial conditions) is always present
+// Example input:
+// .TRAN 40 8E-2 ADMO4 1 UIC
+class Tran : public Statement {
+ public:
+  explicit Tran(amc_float t_stop_s, amc_float t_step_s, int admo_order,
+                int internal_steps);
+  explicit Tran(const std::string& params);
+
+ protected:
+  amc_float t_stop_s;
+  amc_float t_step_s;
+  int admo_order;
+  int internal_steps;
+};
+
+} // namespace amcircuit
 
 #endif //AMCIRCUIT_STATEMENT_H
--- src/Statement.cpp
+++ src/Statement.cpp
@@ -2,4 +2,39 @@
 // Created by Hugo Sadok on 2/6/16.
 //
 
+#include <string>
+
 #include "Statement.h"
+#include "AMCircuitException.h"
+#include "helpers.h"
+
+namespace amcircuit {
+
+Statement::Statement() { }
+
+Statement::Statement(const std::string& params) : line_stream(params) {}
+
+inline Statement::~Statement() { }
+
+Statement::Handler get_statement(std::string params) {
+  if (params[0] == '.') {
+    params = params.substr(1, std::string::npos);
+  }
+  params = params; // TODO str_upper
+  if (params == "TRAN") return Statement::Handler(new Tran(params));
+  throw BadElementString("Invalid string \"" + params + "\"");
+}
+
+Tran::Tran(amc_float t_stop_s, amc_float t_step_s, int admo_order,
+           int internal_steps) : t_stop_s(t_stop_s), t_step_s(t_step_s),
+                                 admo_order(admo_order),
+                                 internal_steps(internal_steps) { }
+
+Tran::Tran(const std::string& params) : Statement(params) {
+  std::string admo_string;
+  line_stream >> t_stop_s >> t_step_s >> admo_string >> internal_steps;
+  admo_order = *admo_string.rbegin() - '0';
+  // line_stream >> std::ws; // ignore whitespace, may be needed...
+}
+
+} // namespace amcircuit
