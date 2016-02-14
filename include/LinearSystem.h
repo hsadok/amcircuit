d4217657ffbb4c988a57295f19cc485108584d65
Hugo Sadok <hugombarreto@gmail.com>
2016-02-14 18:24:36 -0200

    Adding first_index to be able to ignore lines and columns

--- include/LinearSystem.h
+++ include/LinearSystem.h
@@ -10,11 +10,14 @@
 namespace amcircuit {
 
 // Use U for the original matrix as well as the output U from the decomposition
-void lu_decomposition(amc_float **L, amc_float **U, const int size);
+// First index is used to ignore a number of first rows and columns
+void lu_decomposition(amc_float **L, amc_float **U, const int size,
+                      const int first_index = 0);
 
 // C is not an input, it must be a vector allocated with the right size
+// First index is used to ignore a number of first rows and columns
 void solve_lu(amc_float **L, amc_float **U, amc_float *x, amc_float *b,
-             amc_float* c, const int size);
+             amc_float* c, const int size, const int first_index = 0);
 
 }  // namespace amcircuit
 
--- src/LinearSystem.cpp
+++ src/LinearSystem.cpp
@@ -7,18 +7,19 @@
 
 namespace amcircuit {
 // Use U for the original matrix as well as the output U from the decomposition
-void lu_decomposition(amc_float **L, amc_float **U, const int size) {
+void lu_decomposition(amc_float **L, amc_float **U, const int size,
+                      const int first_index) {
   int i, j, k;
 
-  for (i = 0; i < size; ++i){
+  for (i = first_index; i < size; ++i){
     L[i][i] = 1;
     for(k = i+1; k < size; ++k){
       L[k][i] = U[k][i]/U[i][i];
-      for (j = 0; j < size; ++j){
+      for (j = first_index; j < size; ++j){
         U[k][j] = U[k][j] - L[k][i]*U[i][j];
       }
     }
-    for(k = 0; k < i; ++k){
+    for(k = first_index; k < i; ++k){
       L[k][i] = 0;
     }
   }
@@ -26,20 +27,20 @@ void lu_decomposition(amc_float **L, amc_float **U, const int size) {
 
 // C is not an input, it must be a vector allocated with the right size
 void solve_lu(amc_float **L, amc_float **U, amc_float *X, amc_float *B,
-             amc_float* C, const int size) {
+             amc_float* C, const int size, const int first_index) {
   int i, j;
 
   //Lc = b
-  for (i = 0; i < size; ++i){
+  for (i = first_index; i < size; ++i){
     double aux = 0;
-    for(j = 0; j < i; ++j){
+    for(j = first_index; j < i; ++j){
       aux += L[i][j]*C[j];
     }
     C[i] = (B[i]-aux)/L[i][i];
   }
 
   //Ux = c
-  for (i = size - 1; i >= 0; --i){
+  for (i = size - 1; i >= first_index; --i){
     double aux = 0;
     for (j = i+1; j < size; ++j){
       aux += U[i][j]*X[j];
