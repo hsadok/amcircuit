2de18769e13a0d6e4b56436d78ae8a7902be5562
Hugo Sadok <hugombarreto@gmail.com>
2016-02-14 18:42:36 -0200

    Removing debugging string

--- src/AMCircuit.cpp
+++ src/AMCircuit.cpp
@@ -26,7 +26,6 @@ int main(int argc, char const *argv[]) {
   std::string output_file_name;
   if (argc == 2) {
     output_file_name = netlist_file_name + ".out";
-    std::cout << "2!" << std::endl;
   } else {
     output_file_name = argv[2];
   }
